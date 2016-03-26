e49afc407ecd58b093f5656d6c98ca8f29bc229c
Spiderixius <silentwalk15@gmail.com>
2016-03-26 13:21:22 +0100

    Restructed the structure

---
+++ router.php
@@ -0,0 +1,13 @@
+<?php
+
+// router.php that routes any php or html to index.php which will handle it
+// everything else is staticly served
+if (preg_match('/\\.(html|php)$|^[^.]*$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+} else { 
+    echo "<p>404</p>";
+}
+
+require $_SERVER['DOCUMENT_ROOT']. '/index.php'
+
+?>
