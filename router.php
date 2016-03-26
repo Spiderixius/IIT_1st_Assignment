4f59a511fc65ac7fc85ef8ca62bd88c74bb1e7dc
Spiderixius <silentwalk15@gmail.com>
2016-03-26 13:48:00 +0100

    Fixed a minor typo in the router.php file

--- router.php
+++ router.php
@@ -2,12 +2,12 @@
 
 // router.php that routes any php or html to index.php which will handle it
 // everything else is staticly served
-if (preg_match('/\\.(html|php)$|^[^.]*$/', $_SERVER["REQUEST_URI"])) {
+if (!preg_match('/\\.(html|php)$|^[^.]*$/', $_SERVER['REQUEST_URI'])) {
     return false;    // serve the requested resource as-is.
 } else { 
     echo "<p>404</p>";
 }
 
-require $_SERVER['DOCUMENT_ROOT']. '/index.php'
+require $_SERVER['DOCUMENT_ROOT']. '/public/index.php'
 
 ?>
